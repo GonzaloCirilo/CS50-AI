4ead06904ecbbeb3093f9516d45ea85030720498
Gonzalo Cirilo <gonzalo_cirilo@hotmail.com>
2025-09-28 23:46:56 -0500

    minimax algorithm

--- 0 Search/tictactoe/tictactoe.py
+++ 0 Search/tictactoe/tictactoe.py
@@ -47,7 +47,7 @@ def actions(board):
         for j, c in enumerate(rows):  # c stands for cell
             if c == EMPTY:
                 availableCells.append((i, j))
-    raise availableCells
+    return availableCells
 
 
 def result(board, action):
@@ -65,26 +65,111 @@ def result(board, action):
 def winner(board):
     """
     Returns the winner of the game, if there is one.
+    Creating an array to helps check every row, column a diagonal
+    if sum is 3 then X wins, likewise if sum is -3 O wins else None
     """
-    raise NotImplementedError
+    checks = [
+        [(0, 0), (0, 1), (0, 2)],
+        [(1, 0), (1, 1), (1, 2)],
+        [(2, 0), (2, 1), (2, 2)],
+        [(0, 0), (1, 0), (2, 0)],
+        [(0, 1), (1, 1), (2, 1)],
+        [(0, 2), (1, 2), (2, 2)],
+        [(0, 0), (1, 1), (2, 2)],
+        [(2, 0), (1, 1), (0, 2)]
+    ]
+    for check in checks:
+        sum = 0
+        for (i, j) in check:
+            if board[i][j] == X:
+                sum += 1
+            elif board[i][j] == O:
+                sum -= 1
+        if sum == 3:
+            return X
+        elif sum == -3:
+            return O
+
+    return EMPTY
 
 
 def terminal(board):
     """
     Returns True if game is over, False otherwise.
+    Counting every non empty cell. If there is a winner or all cells are counted then we have a terminal state
     """
-    raise NotImplementedError
+    count = 0
+    for r in board:
+        for c in r:
+            if c != EMPTY:
+                count += 1
+    return winner(board) != EMPTY or count == 9
 
 
 def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    raise NotImplementedError
+    winnerResult = winner(board)
+    if winnerResult == X:
+        return 1
+    elif winnerResult == O:
+        return -1
+    else:
+        return 0
+
+
+def maximize(board):
+    if terminal(board):
+        return (utility(board), -1, -1)
+    possibleMoves = actions(board)
+    value = -1e9
+    r = -1
+    c = -1
+    for (i, j) in possibleMoves:
+        newBoard = result(board, (i, j))
+        eval = minimize(newBoard)[0]
+        if value < eval:
+            r = i
+            c = j
+        value = max(eval, value)
+    return (value, r, c)
+
+
+def minimize(board):
+    if terminal(board):
+        return (utility(board), 0, 0)
+    possibleMoves = actions(board)
+    value = 1e9
+    r = -1
+    c = -1
+    for (i, j) in possibleMoves:
+        newBoard = result(board, (i, j))
+        eval = maximize(newBoard)[0]
+        if eval < value:
+            r = i
+            c = j
+        value = min(eval, value)
+    return (value, r, c)
 
 
 def minimax(board):
     """
     Returns the optimal action for the current player on the board.
     """
-    raise NotImplementedError
+    if terminal(board):
+        return EMPTY
+    # Values for alpha beta prunning
+    alpha = -1e9
+    beta = 1e9
+
+    # Get if AI moves as X or O
+    moveToken = player(board)
+
+    # If playing as X AI should we should start maximizing, otherwise we use O and minize,
+    if moveToken == X:
+        result = maximize(board)
+        return (result[1], result[2])
+    else:
+        result = minimize(board)
+        return (result[1], result[2])
