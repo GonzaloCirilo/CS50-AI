83b36ee203a4929c551fe0d894bbb992b856b936
Gonzalo Cirilo <gonzalo_cirilo@hotmail.com>
2025-10-05 23:58:37 -0500

    Knowledge - puzzle

---
+++ 1 Knowledge/knights/puzzle.py
@@ -0,0 +1,94 @@
+from logic import *
+
+AKnight = Symbol("A is a Knight")
+AKnave = Symbol("A is a Knave")
+
+BKnight = Symbol("B is a Knight")
+BKnave = Symbol("B is a Knave")
+
+CKnight = Symbol("C is a Knight")
+CKnave = Symbol("C is a Knave")
+
+# Puzzle 0
+# A says "I am both a knight and a knave."
+knowledge0 = And(
+    Implication(AKnight, Not(AKnave)),
+    Implication(AKnave, Not(AKnight)),
+    # Statement true then A is knight
+    Implication(And(AKnight, AKnave), AKnight),
+    # Statement false then A is knave
+    Implication(Not(And(AKnight, AKnave)), AKnave),
+)
+
+# Puzzle 1
+# A says "We are both knaves."
+# B says nothing.
+knowledge1 = And(
+    Implication(AKnight, Not(AKnave)),
+    Implication(AKnave, Not(AKnight)),
+    Implication(BKnight, Not(BKnave)),
+    Implication(BKnave, Not(BKnight)),
+    # If both are knaves then A is a knight
+    Implication(And(AKnave, BKnave), AKnight),
+    # If they aren't both knaves then A is a knave
+    Implication(Not(And(AKnave, BKnave)), AKnave),
+    Implication(Not(And(AKnave, BKnave)), BKnight),
+)
+
+# Puzzle 2
+# A says "We are the same kind."
+# B says "We are of different kinds."
+knowledge2 = And(
+    Implication(AKnight, Not(AKnave)),
+    Implication(AKnave, Not(AKnight)),
+    Implication(BKnight, Not(BKnave)),
+    Implication(BKnave, Not(BKnight)),
+    Implication(Or(And(BKnight, AKnight), And(BKnave, AKnave)), AKnight),
+    Implication(Not(Or(And(BKnight, AKnight), And(BKnave, AKnave))), AKnave),
+    Implication(Or(And(BKnight, AKnave), And(BKnave, AKnight)), BKnight),
+    Implication(Not(Or(And(BKnight, AKnave), And(BKnave, AKnight))), BKnave),
+)
+
+# Puzzle 3
+# A says either "I am a knight." or "I am a knave.", but you don't know which.
+# B says "A said 'I am a knave'."
+# B says "C is a knave."
+# C says "A is a knight."
+knowledge3 = And(
+    Implication(AKnight, Not(AKnave)),
+    Implication(AKnave, Not(AKnight)),
+    Implication(BKnight, Not(BKnave)),
+    Implication(BKnave, Not(BKnight)),
+    Implication(CKnight, Not(CKnave)),
+    Implication(CKnave, Not(CKnight)),
+    Implication(Or(AKnight, AKnave), AKnight),
+    Implication(Not(Or(AKnight, AKnave)), AKnave),
+    Implication(AKnave, BKnight),
+    Implication(Not(AKnave), BKnave),
+    Implication(CKnave, BKnight),
+    Implication(Not(CKnave), BKnave),
+    Implication(AKnight, CKnight),
+    Implication(Not(AKnight), CKnave),
+)
+
+
+def main():
+    symbols = [AKnight, AKnave, BKnight, BKnave, CKnight, CKnave]
+    puzzles = [
+        ("Puzzle 0", knowledge0),
+        ("Puzzle 1", knowledge1),
+        ("Puzzle 2", knowledge2),
+        ("Puzzle 3", knowledge3)
+    ]
+    for puzzle, knowledge in puzzles:
+        print(puzzle)
+        if len(knowledge.conjuncts) == 0:
+            print("    Not yet implemented.")
+        else:
+            for symbol in symbols:
+                if model_check(knowledge, symbol):
+                    print(f"    {symbol}")
+
+
+if __name__ == "__main__":
+    main()
